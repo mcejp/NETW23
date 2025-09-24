e800bb411220bb504b78887dd379f238e8423e4f
Martin Cejp <cejp.mart@gmail.com>
2025-09-24 15:40:55 -0500

    Reflect improved tilevision packaging

--- netw23.sh
+++ netw23.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-../100worlds/settlements/websocketd \
-    --address=localhost \
-    --port=4000 \
-    --staticdir=../100worlds/settlements/static \
-    env PYTHONPATH=`pwd`/../100worlds/settlements/settlements python -u main.py
+/opt/jupyter-py13/.venv/bin/tilevision-run main.py
